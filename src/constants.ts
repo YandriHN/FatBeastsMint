35417732234d433022c83f9c115ee37f047cf755
YandriHN <87949035+YandriHN@users.noreply.github.com>
2022-10-06 23:42:49 +0700

    mata

--- src/constants.ts
+++ src/constants.ts
@@ -4,4 +4,5 @@ export const startDate = new Date("7 Oct 2022 00:00:00 GMT")
 export const startWlDate = new Date("8 Oct 2022 00:00:00 GMT")
 
 export const mintPrice = '0.5'
+export const matauang = 'SOL'
 export const supply = 2000
