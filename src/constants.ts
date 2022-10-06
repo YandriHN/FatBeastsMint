88186958262a6dab0da03c084706a5d2d09f24ca
YandriHN <87949035+YandriHN@users.noreply.github.com>
2022-10-06 15:02:16 +0700

    2000

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 
 
-export const startDate = new Date("19 May 2022 17:00:00 GMT")
-export const startWlDate = new Date("19 May 2022 16:00:00 GMT")
+export const startDate = new Date("7 Oct 2022 00:00:00 GMT")
+export const startWlDate = new Date("8 Oct 2022 00:00:00 GMT")
 
-export const mintPrice = '0.1'
-export const supply = 10
+export const mintPrice = '0.5'
+export const supply = 2000
