995bbe3607f0d1126544166e7de091d746f3812f
YandriHN <87949035+YandriHN@users.noreply.github.com>
2022-10-08 07:27:46 +0700

    gmt

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 
 
 export const startDate = new Date("9 Oct 2022 04:00:00 GMT")
-export const startWlDate = new Date("8 Oct 2022 04:00:00 GMT")
+export const startWlDate = new Date("8 Oct 2022 00:00:00 GMT")
 
 export const mintPrice = '0.8'
 export const matauang = ("SOL")
